d7b70b405643dda6ef84d9183d70d74f625bf19d
Sanjay Jaiswal <sanjay.jaiswal@cginfinity.com>
2024-01-22 13:08:25 +0530

    add:route

--- src/utilities/Utility.js
+++ src/utilities/Utility.js
@@ -19,6 +19,54 @@ export  const renderComponent = (path) => {
           </>
         );
 
+        case "/useEffect":
+            return (
+              <>
+                {/* please add your UseEffect data here */}
+
+                UseEffect Here 
+              </>
+            );
+
+            case "/useContext":
+                return (
+                  <>
+                    {/* please add your useContext data here */}
+                    useContext Here
+                  </>
+                );
+                case "/useReducer":
+                    return (
+                      <>
+                        {/* please add your useContext data here */}
+                        useReducer Here
+                      </>
+                    );
+                    case "/useMemo":
+                        return (
+                          <>
+                            {/* please add your useContext data here */}
+                                add useMemo here
+                          </>
+                        );
+
+                     
+
+                            case "/customHooks":
+                                return (
+                                  <>
+                                    {/* please add your useContext data here */}
+                                    customHooks Here
+                                  </>
+                                );
+
+                                case "/useRef":
+                                    return (
+                                      <>
+                                        {/* please add your useContext data here */}
+                                        useRef Here
+                                      </>
+                                    );
       default:
         // Render a default component or handle the unknown path case
         return <div>Page not found</div>;
